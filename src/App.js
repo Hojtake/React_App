b530744e38a3e8bfbbc747ea65e1762f501114ab
Hojtake <89243337+Hojtake@users.noreply.github.com>
2021-10-21 15:08:41 +0900

    2021/10/21 15:00 commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
 import "./styles.css";
-import React from "react";
+import React, { useState } from "react";
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "sample1",
+    "sample2"
+  ]);
+
+  const [completeTodos, setCompleteTodos] = useState(["sample3"]);
+
+  const [todoText, setTodoText] = useState([]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") {
+      return;
+    }
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  const
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div class="input-area">
+        <input
+          id="add-text"
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button id="add-button" onClick={onClickAdd}>
+          追加
+        </button>
+      </div>
+
+      <div class="incomplete-area">
+        <p class="title">未完了のTODO</p>
+        <ul id="incomplete-list">
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div class="completed-area">
+        <p class="title">完了したTODO</p>
+        <ul id="complete-list">
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 };
